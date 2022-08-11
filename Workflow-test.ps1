5fc21592455678e8a3046a5a1b5edcf980cfb7b7
Uzair Ansari <uzair_ansari3@hotmail.com>
2022-08-11 17:15:08 +0530

    Adding PS script file

---
+++ Workflow-test.ps1
@@ -0,0 +1,5 @@
+﻿$content = Get-content output.txt
+write-host "Here's the value fetched from output.txt file - $content"
+Write-Host "Present directory - $((pwd).path)"
+get-childitem .\
+($PSVersionTable).os
